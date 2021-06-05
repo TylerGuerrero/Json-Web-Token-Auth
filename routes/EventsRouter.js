f96fbd50f5905adeed06dc2259cbf20dabc7465f
Tyler <guerrero.tyler3@gmail.com>
2021-06-05 00:05:03 -0400

    Added Event routes

--- routes/EventsRouter.js
+++ routes/EventsRouter.js
@@ -21,12 +21,32 @@ router.post('/', async (req, res) => {
     }
 })
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+    const { title, description, date } = req.body
+    const eventId = req.params.id
 
+    const updatedEvent = {}
+    if (title) updatedEvent.title = title 
+    if (description) updatedEvent.description = description
+    if (date) updatedEvent.date = date
+
+    try {
+        const event = await Events.findByIdAndUpdate(eventId, {$set: updatedEvent}, {new: true})
+        return res.status(201).json({event})
+    } catch(err) {  
+        return res.status(500).json({err: err.message})
+    }
 })
 
-router.delete('/:id', (req, res) => {
-    
+router.delete('/:id', async (req, res) => {
+    const eventId = req.params.id
+
+    try {
+        await Events.findByIdAndDelete(eventId)
+        return res.status(201).json({msg: 'Event removed'})
+    } catch (err) {
+        return res.status(500).json({err: err.message})
+    }
 })
 
 module.exports = router
